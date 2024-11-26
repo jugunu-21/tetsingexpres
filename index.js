20d42e4b3b199e4704f712383128e45aa7953622
jugunu-21 <aaruhijugunu@gmail.com>
2024-11-26 16:36:55 +0530

    new commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 const app = express()
 const port = 4000
 app.use("/", (req, res) => {
-    res.json({ message: "heyyy its me " })
+    res.json({ message: "heyyy its me new branch " })
 })
 app.listen(4000, () => {
     console.log("mmmemmemem")
