260349816646cb4e3eae3de29c5c03c78a866010
jugunu-21 <aaruhijugunu@gmail.com>
2024-11-27 11:21:06 +0530

    1

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use('/', (req, res) => {
-    res.json({ message: 'Welcome to URSTYLE Backend API for uu uuudfghvbj uinn me ' });
+    res.json({ message: 'Welcome to URSTYLE Backend API for uu uuudfghvbj uinn me cfgvjhbk' });
 });
 app.listen('1000', () => {
     console.log("dfcgvhbjnkmememmem");
