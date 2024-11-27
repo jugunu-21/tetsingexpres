94f11d26e1fcca6c35860e70bf6619eda4aea891
jugunu-21 <aaruhijugunu@gmail.com>
2024-11-27 11:12:20 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import express, { Express } from 'express'
 const app: Express = express()
 app.use('/', (req, res) => {
-    res.json({ message: 'Welcome to URSTYLE Backend API' });
+    res.json({ message: 'Welcome to URSTYLE Backend API changes' });
 });
 
 app.listen('000', () => {
